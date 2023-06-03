7d17a022bbb69d6511283dc525c9105799df81cd
Ashwani Goswami <ashwanigoswami79@gmail.com>
2023-06-03 17:22:26 +0530

    fix web announcement

--- webview/announcement.js
+++ webview/announcement.js
@@ -1,4 +1,12 @@
 const pool = require("../config/pool");
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+const s3Client = new S3Client({
+  region: process.env.BUCKET_REGION,
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  },
+});
 
 exports.viewWebAnnouncement = async (req, res) => {
     let client
@@ -65,3 +73,50 @@ exports.viewAllWebAnnouncement = async (req, res) => {
         }
     }
 }
+
+exports.viewPDFAnnouncement = async (req, res) => {
+    let client;
+  
+    try {
+      const { aid } = req.params;
+      client = await pool.connect();
+      const check = 'SELECT pdf_path FROM announcement WHERE a_id = $1';
+      const result = await client.query(check, [aid]);
+  
+      if (result.rowCount === 0) {
+        return res.status(404).send({ message: 'Announcement Not Exists' });
+      }
+  
+      const fileUrl = result.rows[0].pdf_path;
+  
+      if (!fileUrl) {
+        return res.status(404).send({ error: 'PDF file not found.' });
+      }
+  
+      const key = 'announcement/' + fileUrl.substring(fileUrl.lastIndexOf('/') + 1); 
+  
+      const getObjectCommand = new GetObjectCommand({
+        Bucket: process.env.BUCKET_NAME,
+        Key: key,
+      });
+  
+      const response = await s3Client.send(getObjectCommand);
+  
+      if (!response.Body) {
+        return res.status(404).send({ error: 'PDF file not found.' });
+      }
+  
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', 'inline; filename=tender.pdf');
+  
+      response.Body.pipe(res);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send({ message: 'Internal Server Error!.' });
+    } finally {
+      if (client) {
+        await client.release();
+      }
+    }
+  };
+  
--- webview/webRoutes/webRoutes.js
+++ webview/webRoutes/webRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { viewAllDetailsFaculty } = require('../../viewList/allview')
-const { viewWebAnnouncement, viewAllWebAnnouncement } = require('../announcement')
+const { viewWebAnnouncement, viewAllWebAnnouncement, viewPDFAnnouncement } = require('../announcement')
 const { viewFacultyByStatus } = require('../facultyView')
 const router = express.Router()
 
@@ -8,4 +8,5 @@ router.get('/webannouncement',viewWebAnnouncement)
 router.get('/facultynsub',viewAllDetailsFaculty)
 router.get('/faculty_members',viewFacultyByStatus)
 router.get('/view_ann_all',viewAllWebAnnouncement)
+router.get('/view_ann/:aid',viewPDFAnnouncement)
 module.exports=router
