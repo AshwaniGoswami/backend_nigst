13248f35db0cff3fd8feb034b46bb1327ebe1db6
Ashwani Goswami <ashwanigoswami79@gmail.com>
2023-06-19 16:04:14 +0530

    album fix

--- controllers/album.js
+++ controllers/album.js
@@ -1,4 +1,6 @@
 const pool = require("../config/pool");
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
 
 exports.createAlbum=async(req,res)=>{
@@ -45,14 +47,50 @@ exports.createAlbum=async(req,res)=>{
     }
   }
 }
+// =====================view==========================
 exports.viewAlbum=async(req,res)=>{
   let connection
   try {
     const{Cname}=req.body;
-    const AllAlbumView="SELECT name,path from album WHERE category_name=$1";
+    const AllAlbumView="SELECT name,path from album ";
     connection=await pool.connect()
-    const allAlbum=await connection.query(AllAlbumView,[Cname])
-    return res.send({data:allAlbum.rows})
+    const allAlbum=await connection.query(AllAlbumView)
+    if (allAlbum.rowCount === 0) {
+      return res.status(404).send({ message: 'No image Found' });
+    }
+    const attachments = allAlbum.rows.map(row => row.path).filter(Boolean);
+const imageData = [];
+
+for (const attachment of attachments) {
+  const fileUrl = attachment;
+  const key = 'gallery/' + fileUrl.substring(fileUrl.lastIndexOf('/') + 1);
+  try {
+ 
+    const s3Client = new S3Client({
+      region: process.env.BUCKET_REGION,
+      credentials: {
+        accessKeyId: process.env.ACCESS_KEY,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY,
+      },
+    });
+
+    const command = new GetObjectCommand({
+      Bucket: process.env.BUCKET_NAME,
+      Key: key,
+    });
+    const url = await getSignedUrl(s3Client, command, { expiresIn: 36000 });
+
+    imageData.push({ fileName: key, url});
+  } catch (error) {
+    console.error(`Error retrieving file '${key}': ${error}`);
+  }
+}
+if (imageData.length === 0) {
+  return res.status(404).send({ error: 'Image  not found.' });
+}
+
+    
+    return res.send({data:imageData})
   } catch (error) {
     console.log(error)
     return res.status(500).send({message:'Internal server error!'})
