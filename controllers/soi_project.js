607ec9d2e485a348f09d1cd4f45c0823178198f1
Ashwani Goswami <ashwanigoswami79@gmail.com>
2023-07-05 12:05:46 +0530

    soi project view web

--- controllers/soi_project.js
+++ controllers/soi_project.js
@@ -103,6 +103,57 @@ exports.viewProject = async (req, res) => {
   }
 };
 
+exports.viewProjectForWeb = async (req, res) => {
+  let connection;
+  try {
+    const allViewProject = "SELECT p_name as name, p_description, path,url, p_id as pid FROM soi_project WHERE visibility=true";
+    connection = await pool.connect();
+    const allProject = await connection.query(allViewProject);
+    if (allProject.rowCount === 0) {
+      return res.status(404).send({ message: 'No image Found' });
+    }
+    const imageData = [];
+
+    for (const row of allProject.rows) {
+      const { name, p_description,path,pid } = row;
+      const fileUrl = path;
+      const key = 'soi_project/' + fileUrl.substring(fileUrl.lastIndexOf('/') + 1);
+
+      try {
+        const s3Client = new S3Client({
+          region: process.env.BUCKET_REGION,
+          credentials: {
+            accessKeyId: process.env.ACCESS_KEY,
+            secretAccessKey: process.env.SECRET_ACCESS_KEY,
+          },
+        });
+
+        const command = new GetObjectCommand({
+          Bucket: process.env.BUCKET_NAME,
+          Key: key,
+        });
+        const url = await getSignedUrl(s3Client, command, { expiresIn: 36000 });
+
+        imageData.push({ name, url,pid,p_description });
+      } catch (error) {
+        console.error(`Error retrieving file '${key}': ${error}`);
+      }
+    }
+    if (imageData.length === 0) {
+      return res.status(404).send({ error: 'Image not found.' });
+    }
+
+    return res.send({ data: imageData });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ message: 'Internal server error!' });
+  } finally {
+    if (connection) {
+      await connection.release();
+    }
+  }
+};
+
 
 
 // ===================update============================
--- webview/webRoutes/webRoutes.js
+++ webview/webRoutes/webRoutes.js
@@ -7,7 +7,7 @@ const { bannerUpload, galleryUpload, SOI_PROJECT_UPLOAD, headerUpload } = requir
 const { FooterCreate, viewFooter, updateFooter, deleteFooter, updateVisible } = require('../footer')
 const { createAlbumCategory, viewAlbumCategory, updateAlbumCategory, deleteAlbumCategory } = require('../../controllers/GalleryCategory')
 const { createAlbum, viewAlbum } = require('../../controllers/album')
-const { createProject, viewProject, updateSoiProject, deleteProject } = require('../../controllers/soi_project')
+const { createProject, viewProject, updateSoiProject, deleteProject, viewProjectForWeb } = require('../../controllers/soi_project')
 const { HeaderCreate, viewHeader, updateVisibility, updateHeader } = require('../header')
 
 const router = express.Router()
@@ -19,29 +19,37 @@ router.get('/view_ann/:aid', viewPDFAnnouncement)
 router.get('/view', viewAllPDFs)
 router.get('/view_archive', viewArchiveToWebsite)
 router.get('/view_archive/:aid', viewArchivePDFAnnouncement)
+
 router.post('/create_banner', bannerUpload, createBanner)
 router.get('/view_banner', getBanner)
 
 
 
 router.delete('/delete_album_category',deleteAlbumCategory)
-router.post('/footer_create', FooterCreate)
-router.get('/footer_view', viewFooter)
-router.patch('/footer_update', updateFooter)
-router.delete('/footer_delete', deleteFooter)
 router.post('/create_album_category', createAlbumCategory)
 router.get('/view_album_category', viewAlbumCategory)
 router.patch('/update_album_category', updateAlbumCategory)
 router.post('/create_album', galleryUpload, createAlbum)
+router.get('/view_album', viewAlbum)
+
+
 router.post('/create_project', SOI_PROJECT_UPLOAD, createProject)
 router.get('/view_project', viewProject)
+router.get('/web_project',viewProjectForWeb)
 router.patch('/update_project', updateSoiProject)
 router.delete('/delete_project', deleteProject)
-router.get('/view_album', viewAlbum)
+
+
 router.post('/create_header', headerUpload, HeaderCreate)
 router.get('/view_header', viewHeader)
 router.patch('/update_header',updateHeader)
-
-router.patch('/update_visible_footer',updateVisible)
 router.patch('/update_visible_header',updateVisibility)
+
+
+router.post('/footer_create', FooterCreate)
+router.get('/footer_view', viewFooter)
+router.patch('/footer_update', updateFooter)
+router.patch('/update_visible_footer',updateVisible)
+router.delete('/footer_delete', deleteFooter)
+
 module.exports = router
